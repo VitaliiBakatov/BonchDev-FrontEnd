83abb20ceea8c606c26585a2fcf015b198e345bc
Vitaly <bakatovvitalij@gmail.com>
2021-03-30 23:59:18 +0300

    fix hw4

--- Hw4/main.js
+++ Hw4/main.js
@@ -13,59 +13,78 @@ const dataFromMars = [
     },
 ]
 
-async function postData(data) {
-    const promise = new Promise((resolve, reject) => {
+function postData(data) {
+    return new Promise((resolve, reject) => {
         setTimeout(() => {
             dataFromMars.push(data)
             resolve()
         }, 1500)
     })
-    const res = await promise
 }
 
 async function getData() {
+
+    const data = await postData(
+        {
+            date: '3 июля 2020 г.',
+            temperature: '-70,7 ° F',
+            windspeed: '11,5 миль/ч',
+            pressure: '766,9  ПА',
+        }
+    )
+
     const promise = new Promise((resolve, reject) => {
         setTimeout(() => {
             try {
                 let i = 1
                 dataFromMars.forEach((dataFromMars) => {
-                    document.getElementById("date" + i).innerHTML = "Дата: " + dataFromMars.date
-                    document.getElementById("temperature" + i).innerHTML = "Температура: " + dataFromMars.temperature
-                    document.getElementById("windspeed" + i).innerHTML = "Скорость ветра: " + dataFromMars.windspeed
-                    document.querySelector('#pressure' + i).innerHTML = "Давление: " + dataFromMars.pressure
-                    document.querySelector('#pressure' + i).insertAdjacentHTML('afterEnd', '<button onclick="btnClick('+i+')">Получить объект</button>')
+                    let container = document.getElementsByClassName("container")
+                    let card = document.createElement("div")
+                    card.setAttribute("class", "card")
+                    container[0].appendChild(card)
+
+                    let h2 = document.createElement("h2")
+                    let date = document.createElement("p")
+                    let temperature = document.createElement("p")
+                    let windspeed = document.createElement("p")
+                    let pressure = document.createElement("p")
+                    let btn = document.createElement("button")
+
+                    h2.textContent = "Данные №" + i
+                    date.textContent = "Дата: " + dataFromMars.date
+                    temperature.textContent = "Температура: " + dataFromMars.temperature
+                    windspeed.textContent = "Скорость ветра: " + dataFromMars.windspeed
+                    pressure.textContent = "Давление: " + dataFromMars.pressure
+                    btn.textContent = "Получить объект"
+                    btn.setAttribute("onclick", "btnClick(" + i + ")")
+
+
+                    card.appendChild(h2)
+                    card.appendChild(date)
+                    card.appendChild(temperature)
+                    card.appendChild(windspeed)
+                    card.appendChild(pressure)
+                    card.appendChild(btn)
+
                     i++
                 })
                 resolve()
             }
             catch (error) {
-                let i = 1
-                console.log(error)
-                dataFromMars.forEach((dataFromMars) => {
-                    document.getElementById("date" + i).innerHTML = "Ошибка..."
-                    document.getElementById("temperature" + i).innerHTML = ""
-                    document.getElementById("windspeed" + i).innerHTML = ""
-                    document.getElementById("pressure" + i).innerHTML = ""
-                    i++
-                })
+                let h2 = document.createElement("h2")
+                h2.textContent = "Ошибка получения данных...."
+                document.getElementsByClassName("container").appendChild(h2)
             }
         }, 1000)
     })
     const res = await promise
 }
 
-function btnClick(i){
-    const {date, ...otherData} =  dataFromMars[i-1]
-    console.log({date, otherData})
+function btnClick(i) {
+    const { date, ...otherData } = dataFromMars[i - 1]
+    console.log({ date, otherData })
 }
 
-postData(
-    {
-        date: '3 июля 2020 г.',
-        temperature: '-70,7 ° F',
-        windspeed: '11,5 миль/ч',
-        pressure: '766,9  ПА',
-    }
-).then(getData).then
+getData()
 
 
